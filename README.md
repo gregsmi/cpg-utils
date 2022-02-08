54ffbc14f816c8532d6fac27ed6ea922e6d2720c
Miah Wander <miah@microsoft.com>
2022-02-08 11:35:56 -0800

    azure implementation

--- README.md
+++ README.md
@@ -14,4 +14,6 @@ To use the library, import functions like this:
 from cpg_utils.cloud import is_google_group_member
 ```
 
+Note: the library depends on the presence of an environment variable CPG_CLOUD. Valid values for this variable are 'azure' and 'gcp'. If the environment variable is not set, or is set to anything other than these two valid values, gcp is assumed.
+
 We use `bumpversion` for incrementing the library's semantic version. A new conda package gets published automatically in the `cpg` conda channel whenever a version bump commit is merged with the `main` branch.
--- cpg_utils/cloud.py
+++ cpg_utils/cloud.py
@@ -1,9 +1,27 @@
 """Convenience functions related to cloud infrastructure."""
 
+import os
 from typing import Optional
 from google.auth import jwt
 from google.cloud import secretmanager
 import google.api_core.exceptions
+import azure.core.exceptions
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+def get_cloud_setting() -> str:
+    """Returns the cloud backend setting, based solely on environment variables
+    
+    Valid values are `gcp` (default) and `azure`."""
+
+    # Default to gcp if no environment variable is set.
+    if 'CPG_CLOUD' not in os.environ:
+        return 'gcp'
+    # Otherwise return azure if selected, then default to gcp.
+    if os.environ['CPG_CLOUD'].lower() == 'azure':
+        return 'azure'
+    else:
+        return 'gcp'
 
 
 def email_from_id_token(id_token: str) -> str:
@@ -13,14 +31,44 @@ def email_from_id_token(id_token: str) -> str:
 
     This function assumes that the token has been verified beforehand."""
 
-    return jwt.decode(id_token, verify=False)['email']
+    # Note, email and upn extracted from the access token are mutable and not always present in
+    # Azure access tokens, depending on the nature of the account for which an access token is being generated.
+    # See the following link for detail:
+    #  - https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-optional-claims
+
+    if get_cloud_setting() == 'azure':
+        decoded = jwt.decode(id_token, verify=False)
+        return decoded['email'] if 'email' in decoded.keys() else decoded['upn']
+    else:
+        jwt.decode(id_token, verify=False)['email']
 
 
 def read_secret(project_id: str, secret_name: str) -> Optional[str]:
-    """Reads the latest version of a GCP Secret Manager secret.
+    """Reads the latest version of a Cloud secret.
 
     Returns None if the secret doesn't exist."""
 
+    if get_cloud_setting() == 'azure':
+        return _read_secret_azure(keyvault_name=project_id, secret_name=secret_name)
+    else:
+        return _read_secret_gcp(project_id=project_id, secret_name=secret_name)
+
+
+def _read_secret_azure(keyvault_name: str, secret_name: str) -> Optional[str]:
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=f"https://{keyvault_name}.vault.azure.net", credential=credential)
+
+    try:
+        return client.get_secret(secret_name).value
+    except azure.core.exceptions.ResourceNotFoundError:
+        # Fail gracefully if no secret of this name exists.
+        return None
+    except azure.core.exceptions.HttpResponseError:
+        # Fail gracefully on other errors.
+        return None
+
+
+def _read_secret_gcp(project_id: str, secret_name: str) -> Optional[str]:
     secret_manager = secretmanager.SecretManagerServiceClient()
     secret_path = secret_manager.secret_path(project_id, secret_name)
 
@@ -41,8 +89,33 @@ def read_secret(project_id: str, secret_name: str) -> Optional[str]:
 
 
 def write_secret(project_id: str, secret_name: str, secret_value: str) -> None:
-    """Adds a new version for a GCP Secret Manager secret and disables all previous versions."""
+    """Sets the new latest version of a Cloud secret."""
 
+    if get_cloud_setting() == 'azure':
+        _write_secret_azure(keyvault_name=project_id, secret_name=secret_name, secret_value=secret_value)
+    else:
+        _write_secret_gcp(project_id=project_id, secret_name=secret_name, secret_value=secret_value)
+
+
+def _write_secret_azure(keyvault_name: str, secret_name: str, secret_value: str) -> None:
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=f"https://{keyvault_name}.vault.azure.net", credential=credential)
+
+    # Set the secret value, this can raise HttpResponse error.
+    new_secret = client.set_secret(
+        name=secret_name, 
+        value=secret_value.encode('UTF-8')
+    )
+
+    # Disable all previous versions.
+    secret_versions = client.list_properties_of_secret_versions(name=secret_name)
+
+    for version in secret_versions:
+        if (version.enabled == True and version.version != new_secret.properties.version):
+            client.update_secret_properties(name=version.name, version=version.version, enabled=False)
+
+
+def _write_scret_gcp(project_id: str, secret_name: str, secret_value: str) -> None:
     secret_manager = secretmanager.SecretManagerServiceClient()
     secret_path = secret_manager.secret_path(project_id, secret_name)
 
