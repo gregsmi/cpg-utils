0e74092261acf2655fb87e69545315f20e416130
Greg Smith <gregsmi@microsoft.com>
2022-10-18 16:37:43 -0700

    fixed better

--- cpg_utils/storage.py
+++ cpg_utils/storage.py
@@ -48,8 +48,8 @@ class DataManagerGCP(DataManager):
 
     def get_dataset_bucket_url(self, dataset: str, bucket_type: str) -> str:
         """Build dataset-specific Hail-style bucket URL for GCP ("gs://...")."""
-        bucket_name = f"{dataset}-{bucket_type}" if dataset else "{bucket_type}"
-        return f"gs://cpg-{bucket_name}"
+        bucket_name = f"cpg-{dataset}-{bucket_type}" if dataset else f"cpg-{bucket_type}"
+        return f"gs://{bucket_name}"
 
     def get_blob(self, dataset: Optional[str], bucket_type: str, blob_path: str) -> Optional[bytes]:
         """Reads a GCP storage bucket blob."""
@@ -90,8 +90,8 @@ class DataManagerAzure(DataManager):
 
     def get_dataset_bucket_url(self, dataset: str, bucket_type: str) -> str:
         """Build dataset-specific Hail-style bucket URL for Azure ("hail-az://...")."""
-        bucket_name = f"{dataset}-{bucket_type}" if dataset else "{bucket_type}"
-        return f"hail-az://{self.get_storage_account(dataset)}/cpg-{bucket_name}"
+        container_name = f"cpg-{dataset}-{bucket_type}" if dataset else f"cpg-{bucket_type}"
+        return f"hail-az://{self.get_storage_account(dataset)}/{container_name}"
 
     def get_blob(self, dataset: Optional[str], bucket_type: str, blob_path: str) -> Optional[bytes]:
         """Reads an Azure storage blob."""
